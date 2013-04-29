72f8419aae1ad99e43d614dbcaf5c3a975148ce2
Dmy <doumengyu@gmail.com>
2013-04-29 10:22:26 +0800

    补齐了第四章部分遗漏的图

--- 鲜活的数据——数据可视化指南代码/第四章：有关时间趋势的可视化/时间中的离散数据/FlowingData订阅者-圆点/FlowingData.R
+++ 鲜活的数据——数据可视化指南代码/第四章：有关时间趋势的可视化/时间中的离散数据/FlowingData订阅者-圆点/FlowingData.R
@@ -1,7 +1,8 @@
 subscribers <- read.csv("http://datasets.flowingdata.com/flowingdata_subscribers.csv")
 
 # plot函数默认的类型就是点状类型(p)
-# plot(subscribers$Subscribers, type="p",ylim=c(0,30000), xlab="Day", ylab="Subscribers")
+plot(subscribers$Subscribers, type="p",ylim=c(0,30000), xlab="Day", ylab="Subscribers")
 
-plot(subscribers$Subscribers, type="h",ylim=c(0,30000), xlab="Day", ylab="Subscribers")
+#类型为h说明要画的是带有高密度的垂直线的散点图
+#plot(subscribers$Subscribers, type="h",ylim=c(0,30000), xlab="Day", ylab="Subscribers")
 points(subscribers$Subscribers, pch = 19, col="black")  # pch指点的大小
--- 鲜活的数据——数据可视化指南代码/第四章：有关时间趋势的可视化/时间中的离散数据/大胃王比赛-堆叠柱形图/Dogs Eating Contest.R
+++ 鲜活的数据——数据可视化指南代码/第四章：有关时间趋势的可视化/时间中的离散数据/大胃王比赛-堆叠柱形图/Dogs Eating Contest.R
@@ -3,4 +3,4 @@ hot_dog_places <- read.csv("http://datasets.flowingdata.com/hot-dog-places.csv",
 # 所以我们要手动将其修改回来
 names(hot_dog_places) <- c("2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010")
 hot_dog_matrix <- as.matrix(hot_dog_places)
-barplot(hot_dog_matrix, border=NA, space=0.25, ylim=c(0,200), xlab="Year", yLab="Hot dogs and buns (HDBs) eaten", main="Hot Dog Eating Contest Results, 2000-2010")
+barplot(hot_dog_matrix, border=NA, space=0.25, ylim=c(0,200), xlab="Year", ylab="Hot dogs and buns (HDBs) eaten", main="Hot Dog Eating Contest Results, 2000-2010")
